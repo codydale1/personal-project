0cc9e41f795393eed3b9a22959dd0c4feeedf2d9
Cody_Bran <bran_nacario@cody.inc>
2024-06-20 11:57:01 +0800

    feat: user authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'user' => \App\Http\Middleware\UserMiddleware::class,
+            'guest' => \App\Http\Middleware\GuestMiddleware::class,
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,17 @@ use App\Http\Controllers\AuthController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('applicants', ApplicantController::class)
-    ->only(['index', 'show']);
-
+Route::middleware('guest')->group(function () {
 Route::get('', fn()=>to_route('auth.create'));
 Route::get('login', fn()=>to_route('auth.create'));
 Route::resource('auth', AuthController::class)
     ->only(['create','store']);
+});
+
+Route::middleware('user')->group(function () {
+    Route::resource('applicants', ApplicantController::class)
+    ->only(['index', 'show']);
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
-
-Route::middleware('auth')->group(function () {
-    // Route::resource('jobs.application', JobApplicationController::class)
-    //     ->only(['create', 'store']);
 });
